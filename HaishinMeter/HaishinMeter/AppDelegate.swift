62397c31b28db364b2a2aa8be48ed7f7d5ca5432
shogo4405 <shogo4405@gmail.com>
2017-12-16 00:19:56 +0900

    1st commit

---
+++ HaishinMeter/HaishinMeter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HaishinMeter
+//
+//  Created by Shogo Endo on 2017/12/16.
+//  Copyright © 2017年 Shogo Endo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HaishinMeter/HaishinMeter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HaishinMeter
+//
+//  Created by Shogo Endo on 2017/12/16.
+//  Copyright © 2017年 Shogo Endo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
+
